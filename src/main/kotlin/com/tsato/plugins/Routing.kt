c08018a16de6974eb677d6b4a523aa62ca3775a3
tsato1 <a8tb1117@gmail.com>
2023-03-11 11:56:30 +0900

    initial commit

---
+++ src/main/kotlin/com/tsato/plugins/Routing.kt
@@ -0,0 +1,10 @@
+package com.tsato.plugins
+
+import io.ktor.server.routing.*
+import io.ktor.server.response.*
+import io.ktor.server.application.*
+
+fun Application.configureRouting() {
+    routing {
+    }
+}
